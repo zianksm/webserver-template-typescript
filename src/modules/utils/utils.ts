21295374619a9f3f86ccd374bb363da9979ed405
zian <mynameiszian@gmail.com>
2022-12-03 16:09:04 +0800

    added fetch utils method

--- src/modules/utils/utils.ts
+++ src/modules/utils/utils.ts
@@ -1,16 +1,29 @@
 import { Config } from "../config/server-config";
 import { JwtHandler, jwtVerifyStatus } from "./jwt";
+import axios from "axios";
 
 export interface ServerUtils {
   fetch<T, U>(
     url: string,
     body: T,
     method: "get" | "post" | "put" | "delete"
-  ): U;
+  ): Promise<U>;
+  createUuid(): string;
   isJWtVerified(token: string): jwtVerifyStatus;
 }
 
-export class Utils {
+export class Utils implements ServerUtils {
   private jwtHandler: JwtHandler;
   private config: Config;
+
+  public async fetch<T, U>(
+    url: string,
+    body: T,
+    method: "get" | "post" | "put" | "delete"
+  ): Promise<U> {
+    const response = await axios[method](url, body);
+    const responseData: U = response.data;
+
+    return responseData;
+  }
 }
