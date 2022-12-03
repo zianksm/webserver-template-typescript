75a19bd9fd03bd70920ad8f7594a32ed78f3a22e
zian <mynameiszian@gmail.com>
2022-12-03 18:18:17 +0800

    adjustment in env and env-loader modules

--- src/modules/config/server-config.ts
+++ src/modules/config/server-config.ts
@@ -8,20 +8,16 @@ export class Config {
   private _slackWebhookUrl: string;
   private _slackIconUrl: string;
   private _port: number;
-  //   private _smtpHost: string;
-  //   private _smptPort: number;
-  //   private _smtpUser: string;
-  //   private _smtpPassword: string;
+  private _smtpHost: string;
+  private _smptPort: number;
+  private _smtpUser: string;
+  private _smtpPassword: string;
   private _slackUsername: string;
   private _dbUrl: string;
   private _dbName: string;
   private _webUrl: string;
-  //   private _baliolaCoreApiUrl: string;
   private _thisServerUrl: string;
-  //   private _pinataApiKey: string;
-  //   private _pinataSecretKey: string;
   private _jwtSecret: string;
-  //   private _betamaxWsUrl: string;
   private _jwtExpiry: number;
   private _slackChannel: string;
 
@@ -33,20 +29,16 @@ export class Config {
     this._slackIconUrl = this.getEnv("SLACK_ICON_URL");
     this._slackChannel = this.getEnv("SLACK_CHANNEL");
     this._port = this.getEnv("PORT", { toNumber: true });
-    // this._smtpHost = this.getEnv("SMTP_HOST");
-    // this._smptPort = this.getEnv("SMTP_PORT", { toNumber: true });
-    // this._smtpUser = this.getEnv("SMTP_USER");
-    // this._smtpPassword = this.getEnv("SMTP_PASS");
+    this._smtpHost = this.getEnv("SMTP_HOST");
+    this._smptPort = this.getEnv("SMTP_PORT", { toNumber: true });
+    this._smtpUser = this.getEnv("SMTP_USER");
+    this._smtpPassword = this.getEnv("SMTP_PASS");
     this._slackUsername = this.getEnv("SLACK_USERNAME");
     this._dbUrl = this.getEnv("DB_URL");
     this._dbName = this.getEnv("DB_NAME");
     this._webUrl = this.getEnv("WEB_URL");
-    // this._baliolaCoreApiUrl = this.getEnv("BALIOLA_URL");
     this._thisServerUrl = this.getEnv("SERVER_URL");
-    // this._pinataApiKey = this.getEnv("PINATA_API_KEY");
-    // this._pinataSecretKey = this.getEnv("PINATA_SECRET_KEY");
     this._jwtSecret = this.getEnv("JWT_SECRET");
-    // this._betamaxWsUrl = this.getEnv("BETAMAX_WEBSOCKET_URL");
   }
 
   private loadEnv() {
@@ -93,21 +85,9 @@ export class Config {
     return this._jwtExpiry;
   }
 
-  //   public get betamaxWsUrl(): string {
-  //     return this._betamaxWsUrl;
-  //   }
-
   public get jwtSecret(): string {
     return this._jwtSecret;
   }
-
-  //   public get pinataApiKey(): string {
-  //     return this._pinataApiKey;
-  //   }
-
-  //   public get pinataSecretKey(): string {
-  //     return this._pinataSecretKey;
-  //   }
 
   public get nodeEnv(): string {
     return this._nodeEnv;
@@ -125,21 +105,21 @@ export class Config {
     return this._port;
   }
 
-  //   public get smtpHost(): string {
-  //     return this._smtpHost;
-  //   }
+  public get smtpHost(): string {
+    return this._smtpHost;
+  }
 
-  //   public get smptPort(): number {
-  //     return this._smptPort;
-  //   }
+  public get smptPort(): number {
+    return this._smptPort;
+  }
 
-  //   public get smtpUser(): string {
-  //     return this._smtpUser;
-  //   }
+  public get smtpUser(): string {
+    return this._smtpUser;
+  }
 
-  //   public get smtpPassword(): string {
-  //     return this._smtpPassword;
-  //   }
+  public get smtpPassword(): string {
+    return this._smtpPassword;
+  }
 
   public get slackUsername(): string {
     return this._slackUsername;
@@ -156,10 +136,6 @@ export class Config {
   public get webUrl(): string {
     return this._webUrl;
   }
-
-  //   public get baliolaCoreApiUrl(): string {
-  //     return this._baliolaCoreApiUrl;
-  //   }
 
   public get thisServerUrl(): string {
     return this._thisServerUrl;
