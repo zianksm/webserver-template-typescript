82bae2438a2b2d4fb8b6c84200ddeafac6e91c13
zian <mynameiszian@gmail.com>
2022-12-03 16:25:08 +0800

    add method placeholder

--- src/modules/utils/jwt.ts
+++ src/modules/utils/jwt.ts
@@ -1,3 +1,6 @@
+import jwt from "jsonwebtoken";
+import { Config } from "../config/server-config";
+
 // TODO : discuss jwt payload
 export type jwtPayload = {};
 export type jwtVerifyStatus = {
@@ -12,4 +15,22 @@ export interface JwtHandler {
   verify(token: string): jwtVerifyStatus;
 }
 
-export class Jwt {}
+export class Jwt implements JwtHandler {
+  private config: Config;
+  private secret: string;
+
+  constructor(config: Config) {
+    this.config = config;
+    this.secret = config.jwtSecret;
+  }
+
+  public create(payload: jwtPayload): string {
+    return jwt.sign(payload, this.secret);
+  }
+
+  public getExpiry(): string {}
+
+  public decode(token: string): jwtPayload {}
+
+  public verify(token: string): jwtVerifyStatus {}
+}
